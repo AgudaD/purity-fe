b4ac0da0010ac51a5d1fea3104db8935a7c25005
AgudaD <agudaoluwanifemi@gmail.com>
2024-11-05 20:45:52 +0100

    new purity ui project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-300">
+      <div className="bg-blue-300 p-12 rounded-md shadow-lg drop-shadow-lg">
+        <p className="text-lg font-semibold">Hello world</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
